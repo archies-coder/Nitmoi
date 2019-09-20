4c6e0769dfdbca8c625775e592bcbae21248181c
archies-coder <archisbhoir2016@gmail.com>
2019-09-21 00:12:23 +0530

    deployment tweak 21/9/2019

--- app/config/keys.js
+++ app/config/keys.js
@@ -1,4 +1,4 @@
-const dbURI = 'mongodb://localhost:27017/nitmoi'
+const dbURI = 'mongodb://archisbhoir:Archi%40123321@ds351807.mlab.com:51807/nitmoi'
 const SECRET = 'fdshgchsbfojsd153456ds4fndsjbvjkdsda4sfdfgf564sd56v4f';
 module.exports = {dbURI, SECRET}
 
